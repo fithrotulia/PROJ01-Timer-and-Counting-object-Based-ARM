7e31685d4c39d42b98eaf7a24f65daa738575eb7
pratamaizzat <pratama.izzat231298@gmail.com>
2019-11-16 18:13:38 +0700

    memeperbaiki bug.

--- Src/Taskku.c
+++ Src/Taskku.c
@@ -17,15 +17,15 @@ uint16_t Dark[3];
 uint16_t Light[3]={4096, 4096, 4096};
 uint16_t Thrs[3];
 
-uint16_t milisecond, second, minute;
+uint16_t milisecond=0, second=0, minute=0;
 uint64_t counter_gap=0;
 uint16_t lapA_milis=0, lapA_Sec=0, lapA_minute=0;
 uint16_t lapB_milis=0, lapB_Sec=0, lapB_minute=0;
 uint16_t lapC_milis=0, lapC_Sec=0, lapC_minute=0;
-uint8_t LAP_A, LAP_B, LAP_C;
+uint8_t LAP_A=0, LAP_B=0, LAP_C=0;
 
 uint16_t refreshDisplay;
-uint16_t timeOut1, timeOut2, timeOut3, timeOut4, timeOut5, timeOutVal = 700;
+uint16_t timeOut1=0, timeOut2=0, timeOut3=0, timeOut4=0, timeOut5=0, timeOutVal = 700;
 unsigned char bouncing1=0xFF;
 unsigned char bouncing2=0xFF;
 unsigned char bouncing3=0xFF;
@@ -82,7 +82,7 @@ void Task_Run_Main(void)
 
 	/*	Reset Button */
 	if(Reset_On){
-		if(timeOut2++ > timeOutVal)
+		if(timeOut2 > timeOutVal)
 		{
 			stopwatchEnable=0;
 			UART_Println("PUSH BUTTON RESET ERROR!");
